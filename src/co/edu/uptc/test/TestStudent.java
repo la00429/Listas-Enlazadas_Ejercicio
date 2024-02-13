653faacec455e7903f4e498bcef4bd7f08a8b486
Laura Figueredo <112426346+la00429@users.noreply.github.com>
2024-02-13 16:46:38 -0500

    pruebas en lista enlazada con objetos de tipo Student

--- src/co/edu/uptc/test/TestStudent.java
+++ src/co/edu/uptc/test/TestStudent.java
@@ -7,9 +7,21 @@ public class TestStudent {
 
 	public static void main(String[] args) {
 		Student student1 = new Student("Laura", 20);
+		Student student2 = new Student("Vanessa", 10);
+		Student student3 = new Student("Sofía", 19);
 		SimpleList<Student> listStudent = new SimpleList<Student>();
 		listStudent.insert(student1);
+		listStudent.insert(student2);
+		listStudent.insert(student3);
 		System.out.println(listStudent.show());
+		System.out.println(listStudent.exist(student2));
+		listStudent.remove(student1);
+		System.out.println(listStudent.show());
+		listStudent.remove(student3);
+		System.out.println(listStudent.show());
+		
+		
+		
 
 	}
 
